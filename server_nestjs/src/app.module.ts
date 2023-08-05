14e07b92575a78cd463be27b16ed736f73ac9e4c
Semira <semira.linden@ice.rwth-aachen.de>
2023-08-05 20:51:20 +0200

    #14 Erste Funktionen im Service, Namensänderungen im schema
    Die Funktionen wurden noch nicht getestet und müssen ohnehin auf die noch zu erstellenden Interfaces angepasst werden, aber das Grundgerüst ist jetzt da.

--- server_nestjs/src/app.module.ts
+++ server_nestjs/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphModule } from './graph/graph.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [GraphModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ server_nestjs/src/graph/graph.service.ts
@@ -0,0 +1,101 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class GraphService {
+
+    constructor(private prisma: PrismaService) { }
+
+    /**
+     * creates a new concept node and returns it
+     * @param parentId 
+     * @param conceptName 
+     */
+    async createConcept(parentId: number, conceptName: string) {
+        const newConcept = await this.prisma.concept.create({
+            data: {
+                name: conceptName,
+                description: '',
+                myParents: {
+                    create: [
+                        {
+                            parent: {
+                                connect: {
+                                    id: parentId
+                                }
+                            }
+                        }
+                    ]
+                }
+            }
+        });
+    }
+
+    /**
+     * creates a new concept edge and returns it
+     * @param parentId
+     * @param prerequisiteId
+     * @param successorId
+     * @returns the new concept edge
+     */
+    async createConceptEdge(parentId: number, prerequisiteId: number, successorId: number) {
+        // check if edge already exists
+        const edge = await this.prisma.conceptEdge.findUnique({
+            where: {
+                parentId: parentId,
+                prerequisiteId: prerequisiteId,
+                successorId: successorId
+            }
+        });
+        if (edge) {
+            return edge;
+        }
+
+        // create new edge
+        const newEdge = await this.prisma.conceptEdge.create({
+            data: {
+                prerequisite: {
+                    connect: {
+                        id: prerequisiteId
+                    }
+                },
+                successor: {
+                    connect: {
+                        id: successorId
+                    }
+                },
+                parent: {
+                    connect: {
+                        id: parentId
+                    }
+                }
+            }
+        });
+        return newEdge;
+    }
+
+    /**
+     * deletes a childless concept
+     * @param conceptNodeId 
+     * @returns 
+     */
+    async deleteConceptNode(conceptNodeId: number): Promise<any> {
+        // can only delete concept nodes that have no children
+        return await this.prisma.conceptNode.delete({
+            where: { id: conceptNodeId },
+        });
+    }
+
+    /**
+     * deletes a concept edge
+     * @param conceptEdgeId 
+     * @returns 
+     */
+    async deleteConceptEdge(conceptEdgeId: number): Promise<any> {
+        return await this.prisma.conceptEdge.delete({
+            where: { id: conceptEdgeId },
+        });
+    }
+
+}
