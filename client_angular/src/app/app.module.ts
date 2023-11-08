f0310c60a98cd660ab0c3eff5cf51b2fca28077b
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-11-08 19:32:27 +0100

    Chatbot Markdown support
    - Footnotes not implemented by most libaries because its not standart (just github flavored)
    - Need to get extension working: https://github.com/bent10/marked-extensions/tree/main/packages/footnote

--- client_angular/src/app/app.module.ts
+++ client_angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './Modules/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MarkdownModule } from 'ngx-markdown';
 // Components
 import { GraphComponent } from './Pages/graph/graph.component';
 import { ContentBoardComponent } from './Pages/contentBoard/contentBoard.component';
@@ -71,7 +72,8 @@ import { ChatBotDialogComponent } from './Pages/chat-bot/chat-bot-dialog/chat-bo
     BrowserAnimationsModule,
     MaterialModule,
     NgxExtendedPdfViewerModule,
-    HttpClientModule
+    HttpClientModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
