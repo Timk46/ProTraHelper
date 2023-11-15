e7bc95cee7ba0530b5ad50bb934ed2e52f31aec7
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-11-15 08:26:55 +0100

    Added RN I Videos to seed.ts
    You must have a folder server_nestjs/src/storage/RNI with all the video files for

--- server_nestjs/prisma/seed.ts
+++ server_nestjs/prisma/seed.ts
@@ -466,6 +466,427 @@ async function main() {
     );
   }
 
+  console.log("Importing RN I Videos as files")
+
+  await prisma.file.create({
+    data: {
+      name: '01-Organisation.mp4',
+      uniqueIdentifier: '4b15d033-a8bd-4586-82dd-6f35e1e87ec7',
+      path: 'RNI/01-Organisation.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '1-1-Einfuehrung.mp4',
+      uniqueIdentifier: 'ddfe6040-9b88-4ac5-8f0a-90046d80cb17',
+      path: 'RNI/1-1-Einfuehrung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '1-2-Strukturen.mp4',
+      uniqueIdentifier: 'e576ba76-5136-483f-92f6-dd3ffe9df9c0',
+      path: 'RNI/1-2-Strukturen.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '1-3-Vermittlung.mp4',
+      uniqueIdentifier: '655d268e-1ae4-4153-b53e-80fc2177ced4',
+      path: 'RNI/1-3-Vermittlung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '1-4-Anforderungen.mp4',
+      uniqueIdentifier: 'd4cc64d6-59e5-431c-9b05-1c4cb8d9c255',
+      path: 'RNI/1-4-Anforderungen.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-1-Sicherheitsanforderungen.mp4',
+      uniqueIdentifier: 'df8df04e-6e35-41be-afa4-6dbaa1cc2565',
+      path: 'RNI/10-1-Sicherheitsanforderungen.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-2-AngriffeInternet.mp4',
+      uniqueIdentifier: 'c612bf5f-1ff2-4734-af05-08450d83b256',
+      path: 'RNI/10-2-AngriffeInternet.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-3-Kryptographie.mp4',
+      uniqueIdentifier: '76dc2c07-14fe-459e-9904-8cd3f958e400',
+      path: 'RNI/10-3-Kryptographie.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-4-Sicherheitsmechanismen1.mp4',
+      uniqueIdentifier: '35bc0e9e-d4b5-41be-876a-3aa97ba75f1b',
+      path: 'RNI/10-4-Sicherheitsmechanismen1.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-4-Sicherheitsmechanismen2.mp4',
+      uniqueIdentifier: 'c644172f-3aa8-4b8c-ba00-4572da9a6b14',
+      path: 'RNI/10-4-Sicherheitsmechanismen2.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-5-Beispiele.mp4',
+      uniqueIdentifier: 'cc478230-6a22-4354-a773-89605f45a7cc',
+      path: 'RNI/10-5-Beispiele.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '10-6-Firewalls.mp4',
+      uniqueIdentifier: '4a7869b1-7cfb-42cc-9305-84abafac740c',
+      path: 'RNI/10-6-Firewalls.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '11-1-Probeklausur.mp4',
+      uniqueIdentifier: '27c37247-50e7-40a6-9a1f-e7b122118cdb',
+      path: 'RNI/11-1-Probeklausur.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '2-1-Einfuehrung.mp4',
+      uniqueIdentifier: '5fdf6d8d-8641-42d8-9448-1455c32499c6',
+      path: 'RNI/2-1-Einfuehrung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '2-2-Protokolle.mp4',
+      uniqueIdentifier: '346874fc-7392-4300-94fc-b3cd2c8e9a56',
+      path: 'RNI/2-2-Protokolle.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '2-3-OSIModell.mp4',
+      uniqueIdentifier: 'dd36b284-4a28-4f03-ab4d-23c0dd0587b8',
+      path: 'RNI/2-3-OSIModell.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '2-4_InternetModell.mp4',
+      uniqueIdentifier: '99efa7c7-e32e-4673-a327-6f8404500981',
+      path: 'RNI/2-4_InternetModell.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-1-Hardware.mp4',
+      uniqueIdentifier: '46c82856-d93f-49fc-ab80-a3cbd4d9b508',
+      path: 'RNI/3-1-Hardware.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-2-Modulation.mp4',
+      uniqueIdentifier: 'fc254e51-8ca3-487f-b1ff-bd1fca5692f7',
+      path: 'RNI/3-2-Modulation.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-3-Codierung.mp4',
+      uniqueIdentifier: '40bdf18b-8e6b-4caa-bad3-e2f234ae5d6b',
+      path: 'RNI/3-3-Codierung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-4-Framing.mp4',
+      uniqueIdentifier: 'fa031498-3d8d-4b9b-8b01-cdf2503aea67',
+      path: 'RNI/3-4-Framing.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-5-Fehlererkennung.mp4',
+      uniqueIdentifier: '0ac3b7ae-9134-4485-aaaf-e97d1d09599a',
+      path: 'RNI/3-5-Fehlererkennung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-6-Ethernet.mp4',
+      uniqueIdentifier: 'd0186d23-18cc-4d01-bbbd-ad9d16b832e0',
+      path: 'RNI/3-6-Ethernet.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '3-7-MAC.mp4',
+      uniqueIdentifier: 'bac75908-48b1-4686-9e2c-038f0a0b26ef',
+      path: 'RNI/3-7-MAC.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '4-1-Vermittlung.mp4',
+      uniqueIdentifier: '0bdb47cf-3565-4982-bb8a-731806f6cc0a',
+      path: 'RNI/4-1-Vermittlung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '4-2-Switches.mp4',
+      uniqueIdentifier: '9c0e82f2-59da-4ec5-93e9-d772618e4669',
+      path: 'RNI/4-2-Switches.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '4-3-SpanningTree.mp4',
+      uniqueIdentifier: '5b5753ba-5e83-4106-b83c-ba934930b08f',
+      path: 'RNI/4-3-SpanningTree.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '4-4-VLAN.mp4',
+      uniqueIdentifier: 'e25ca2cc-9c5b-455b-a032-03f3b002c60e',
+      path: 'RNI/4-4-VLAN.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-1-IPGrundlagen.mp4',
+      uniqueIdentifier: '6e2acca6-788c-4883-a4d7-ac7e9b8cb4d3',
+      path: 'RNI/5-1-IPGrundlagen.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-10-IPv6.mp4',
+      uniqueIdentifier: 'c0c85cf5-5087-4f11-9098-7229a0ea999e',
+      path: 'RNI/5-10-IPv6.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-2-IPAdressierung.mp4',
+      uniqueIdentifier: '6321e4b3-41b7-49a1-9d8f-775385d9d6c0',
+      path: 'RNI/5-2-IPAdressierung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-3-IPWeiterleitung.mp4',
+      uniqueIdentifier: '5ddd84c2-bfe0-4457-8375-2914091e6494',
+      path: 'RNI/5-3-IPWeiterleitung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-4-IPSubnetting.mp4',
+      uniqueIdentifier: '012f7741-fe8c-4b86-938d-2da121df3641',
+      path: 'RNI/5-4-IPSubnetting.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-5-IPHeader.mp4',
+      uniqueIdentifier: '9097aa64-452a-43f6-870d-22446d4206a2',
+      path: 'RNI/5-5-IPHeader.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-6-FragmentierungICMP.mp4',
+      uniqueIdentifier: '09893709-9e7c-45c3-93de-8f13e55f7e1d',
+      path: 'RNI/5-6-FragmentierungICMP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-7-ARP.mp4',
+      uniqueIdentifier: 'c3173208-30ff-4443-ab84-501411b7aba3',
+      path: 'RNI/5-7-ARP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '5-8-DHCP.mp4',
+      uniqueIdentifier: '091a18c6-78b8-4fce-bf77-c2aa0cc83f9b',
+      path: 'RNI/5-8-DHCP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '6-1-Routing.mp4',
+      uniqueIdentifier: 'c7f6e114-471b-4f86-9673-f38cd4a787ce',
+      path: 'RNI/6-1-Routing.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '6-2-DistanceVector.mp4',
+      uniqueIdentifier: '73729652-d618-411f-994a-e4927822767e',
+      path: 'RNI/6-2-DistanceVector.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '6-3-LinkState.mp4',
+      uniqueIdentifier: '76faac70-42aa-4657-9efa-0e0944020387',
+      path: 'RNI/6-3-LinkState.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '6-4-BGP.mp4',
+      uniqueIdentifier: '9809f209-9996-40b5-b167-fc6465010b53',
+      path: 'RNI/6-4-BGP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-1-EndeZuEnde.mp4',
+      uniqueIdentifier: '7a3f8d6e-7298-4b69-a16f-8ea0e216970a',
+      path: 'RNI/7-1-EndeZuEnde.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-2-UDP.mp4',
+      uniqueIdentifier: '5cf70ec0-bcdf-4b1a-a3e2-fa1601819118',
+      path: 'RNI/7-2-UDP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-3-TCP.mp4',
+      uniqueIdentifier: 'a6e7b411-69e7-4154-93b5-9a4892514210',
+      path: 'RNI/7-3-TCP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-4-TCPVerbindungsaufbau.mp4',
+      uniqueIdentifier: '7d6e0ca6-74d4-46d5-9c3a-8a3a9fd943b2',
+      path: 'RNI/7-4-TCPVerbindungsaufbau.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-5-StopAndWait.mp4',
+      uniqueIdentifier: '4eccb8d8-92e2-4e5c-a7b5-b67c9a6d0616',
+      path: 'RNI/7-5-StopAndWait.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-6-SlidingWindow.mp4',
+      uniqueIdentifier: 'cf8adaf7-9d4c-4384-b8a4-4045f34fd302',
+      path: 'RNI/7-6-SlidingWindow.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '7-7-TCPSicherung.mp4',
+      uniqueIdentifier: '361b847d-8add-406e-860e-b634026aa2d9',
+      path: 'RNI/7-7-TCPSicherung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '8-1-Datendarstellung.mp4',
+      uniqueIdentifier: '27ddb422-81aa-4e34-9c52-2fbce00efda4',
+      path: 'RNI/8-1-Datendarstellung.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '9-1-SMTPundHTTP.mp4',
+      uniqueIdentifier: '601c72cf-8c8d-4af0-80b0-062fd506654d',
+      path: 'RNI/9-1-SMTPundHTTP.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: '9-2-DNS.mp4',
+      uniqueIdentifier: '97b280e3-51d2-4ee3-b37c-e6179b3de060',
+      path: 'RNI/9-2-DNS.mp4',
+      type: "Video",
+    },
+  });
+await prisma.file.create({
+    data: {
+      name: 'RNimport.py',
+      uniqueIdentifier: 'e08ba1ec-5241-40f0-ac29-e837fbc63c84',
+      path: 'RNI/RNimport.py',
+      type: "Video",
+    },
+  });
+
+
+
   console.log('Creating rest from Seed.ts...');
 
   // update user to have a current concept node
