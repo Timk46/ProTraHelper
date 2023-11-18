c59089626bc6371ac0bb3fd3ddb3d4bf9e34b48d
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-11-18 13:47:04 +0100

    Display Role in Toolbar

--- client_angular/src/app/app.component.ts
+++ client_angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { UserService } from './Services/auth/user.service';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { globalRole } from '@DTOs/roles.enum';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   userMail: string = "";
+  userRole: string = "";
   userIsLoggedIn: boolean = false;
   constructor
     (
@@ -20,6 +22,7 @@ export class AppComponent {
       this.userService.isAuthenticated$.subscribe((isAuthenticated) => {
         this.userIsLoggedIn = isAuthenticated;
         this.userMail= userService.getEmail();
+        this.userRole = userService.getRole();
       });
     }
 
