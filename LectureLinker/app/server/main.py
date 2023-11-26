421d8663cd3c0e1c85691faa82c103d91764c690
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-11-26 18:07:08 +0100

    Lecture Linker: Streaming Answer works
    - current problem: uft-8 / iso encoding
    - took over 10 hours because there a lot of bad libaries and langchain documentation is bad....
    - helpful video: https://www.youtube.com/watch?v=y2cRcOPHL_U&ab_channel=JamesBriggs

--- LectureLinker/app/server/main.py
+++ LectureLinker/app/server/main.py
@@ -1,107 +1,98 @@
-# Start with uvicorn chatbot_server:app --reload
-from fastapi import FastAPI
-from pydantic import BaseModel
 import os
-from typing import List, Dict
+import asyncio
+from typing import Any
+import uvicorn
 
-from fastapi import FastAPI, Body,  HTTPException
-from fastapi.responses import JSONResponse, StreamingResponse
-from langchain.agents import AgentType, initialize_agent, load_tools
-from langchain.callbacks.streaming_stdout_final_only import (
-    FinalStreamingStdOutCallbackHandler,
-)
+from pydantic import BaseModel
+
+from fastapi import FastAPI, Body, HTTPException
+from fastapi.responses import StreamingResponse
+from fastapi import FastAPI
+
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.streaming_aiter import AsyncIteratorCallbackHandler
+from langchain.schema import LLMResult
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.pgvector import PGVector
 from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains.query_constructor.base import AttributeInfo
-import langchain 
-langchain.debug = True  # Super praktisch: Finales Prompt wird ausgegeben!
-
+#import langchain 
+#langchain.debug = True  # Super praktisch: Finales Prompt wird ausgegeben!
 
 app = FastAPI()
 
 os.environ["OPENAI_API_KEY"]="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"
 
-llm = ChatOpenAI(
-    callbacks=[FinalStreamingStdOutCallbackHandler()],
-    temperature = 0.0, 
+class AsyncCallbackHandler(AsyncIteratorCallbackHandler):
+    content: str = ""
+    final_answer: bool = False
+    
+    def __init__(self) -> None:
+        super().__init__()
+
+    async def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
+        if token is not None and token != "":
+            print(token)
+            self.queue.put_nowait(token)
+            
+    async def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        self.done.set()
+
+    async def on_llm_error(self, error: BaseException, **kwargs: Any) -> None:
+        self.done.set()
+         
+
+async def run_call(query: str, stream_it: AsyncCallbackHandler):
+    llm = ChatOpenAI(
+    openai_api_key=os.getenv("OPENAI_API_KEY"),
+    temperature=0.0,
     model = 'gpt-4-1106-preview',
-    verbose=True
-    )
-
-connection_string = PGVector.connection_string_from_db_params(
-     driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"),
-     host=os.environ.get("PGVECTOR_HOST", "vectordb.bshefl0.bs.informatik.uni-siegen.de"),
-     port=int(os.environ.get("PGVECTOR_PORT", "3306")),
-     database=os.environ.get("PGVECTOR_DATABASE", "vectordb"),
-     user=os.environ.get("PGVECTOR_USER", "root"),
-     password=os.environ.get("PGVECTOR_PASSWORD", "qzx5vQG9WQ2b35eZUWujPUhVb8xRr"),
- )
-
-documents = []
-CONNECTION_STRING = connection_string
-COLLECTION_NAME = "RN1_chunksize1500overlap225"  ## OFP = ChunkSize1500overlap225 ; RNI = RN1chunksize1500overlap225
-embeddings = OpenAIEmbeddings()
-
-store = PGVector(
-    collection_name=COLLECTION_NAME,
-    connection_string=CONNECTION_STRING,
-    embedding_function=embeddings,
+    streaming=True,  # ! important
+    callbacks=[]  # ! important (but we will add them later)
 )
 
-class QuestionInput(BaseModel):
-    prompt: str
-    lecture: str
-    
-@app.post("/ask", response_class=JSONResponse)
-async def getQa(questionInput: QuestionInput):
-    lecture = questionInput.lecture
-    prompt = questionInput.prompt
+    connection_string = PGVector.connection_string_from_db_params(
+        driver=os.environ.get("PGVECTOR_DRIVER", "psycopg2"),
+        host=os.environ.get("PGVECTOR_HOST", "vectordb.bshefl0.bs.informatik.uni-siegen.de"),
+        port=int(os.environ.get("PGVECTOR_PORT", "3306")),
+        database=os.environ.get("PGVECTOR_DATABASE", "vectordb"),
+        user=os.environ.get("PGVECTOR_USER", "root"),
+        password=os.environ.get("PGVECTOR_PASSWORD", "qzx5vQG9WQ2b35eZUWujPUhVb8xRr"),
+    )
 
-    # Verwenden Sie 'await' nur, wenn die Funktion tatsächlich eine Coroutine zurückgibt
-    response = await getAnswer(lecture, prompt)
-    return JSONResponse(content={
-            "result": response,
-        })
+    documents = []
+    CONNECTION_STRING = connection_string
+    COLLECTION_NAME = "RN1_chunksize1500overlap225"  ## OFP = ChunkSize1500overlap225 ; RNI = RN1chunksize1500overlap225
+    embeddings = OpenAIEmbeddings()
 
-
-
-async def getAnswer(lecture: str, prompt: str):
-    match lecture:
-        case "RN1":
-            store.collection_name = "RN1_chunksize1500overlap225"
-        case "OFP":
-            store.collection_name = "OFP_ChunkSize1500overlap225"
-        case _:
-            # Default. Falls die Lecture nicht existiert, wird ein entsprechender Fehler zurückgegeben
-            raise HTTPException(status_code=404, detail="Lecture does not exist")
+    store = PGVector(
+        collection_name=COLLECTION_NAME,
+        connection_string=CONNECTION_STRING,
+        embedding_function=embeddings,
+    )
 
     metadata_field_info = [
-        AttributeInfo(
-            name="source",
-            description="Der Dateiname, aus welchem der Content extrahiert wurde.",
-            type="string",
-        )
-    ]
-
-    question = prompt
-    print (question)
+            AttributeInfo(
+                name="source",
+                description="Der Dateiname, aus welchem der Content extrahiert wurde.",
+                type="string",
+            )
+        ]
 
     template = """Du bist ein hilfreicher Tutor und kannst sehr gut erklären. Gegeben sind die folgenden extrahierten Teile eines Vorlesungstranskripts und eine Frage, erstelle eine finale Antwort mit Verweisen ("QUELLEN"). Wenn du die Antwort nicht weißt, sage einfach, dass du es nicht weißt. Versuche nicht, eine Antwort zu erfinden. Du erhälst dazu merhfach Informationen aus Vorlesungstranskripten in folgendem Format:
     Content: Ausschnitt aus dem Vorlesungstranskript, welchen du referenzieren kannst. Zu einem Content gehört immer der Source, welches auf ihn folgt.
     Source: filename: Der Dateiname timestamp: Der timestamp aus den Metadaten. 
-    
+
     Für alles, was du den contents benutzt, musst du den passenden filename und timestamp aus dem Source wie folgt hinzufügen: ^[[FILENAME.mp4 bei Stelle](/video?fileName=FILENAME.mp4&timeStamp=TIMESTAMP)]
 
     Ein Beispiel:
     Content: unterschiedlichen Fragenformate auch mal zu zeigen. Der Hauptteil der Klausur ist aber nach wie vor Freitext. Das heißt, es gibt auch ganz viele Freitextaufgaben, allerdings normalerweise wirklich nur mit ein paar Zeilen Text, also ein paar Sätzen, die Sie angeben müssen. Beispielsweise hier beschreiben Sie kurz die grundsätzliche Vorgehensweise bei der 4b5b-Codierung und ich gebe Ihnen häufig dann noch in Klammern Erklärungen an, was ich jetzt genau von Ihnen wissen will. Die bitte genau lesen. Source: filename: 11-1-Probeklausur.srt timestamp: 00:26:15,000
     Beispiel-Antwort: In der Klausur werden hauptsächlich Freitextaufgaben gestellt, bei denen Sie in der Regel nur ein paar Sätze schreiben müssen. Zum Beispiel könnten Sie aufgefordert werden, die grundsätzliche Vorgehensweise bei der 4b5b-Codierung zu beschreiben, wobei in Klammern zusätzliche Erklärungen angegeben sein können, die genau lesen sollten  ^[[11-1-Probeklausur.mp4 an Stelle: 00:26:15,000](/video?fileName=11-1-Probeklausur.mp4&timeStamp=00:26:15,000)].
-    
+
     Verfahre für folgende Frage nach dem gleichen Schema:
-    
+
         Frage: {question}
         =========
         {summaries}
@@ -113,23 +104,44 @@ async def getAnswer(lecture: str, prompt: str):
 
     chain_type_kwargs = {"prompt": PROMPT}
 
+    llm2 = llm
+    llm2.callbacks = [stream_it]
     qa_chain = RetrievalQAWithSourcesChain.from_chain_type(
-        llm=llm,
+        llm=llm2,
         chain_type="stuff",
-        retriever=store.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .7, "k": 15}, metadata_field_info=metadata_field_info), # https://python.langchain.com/docs/modules/data_connection/retrievers/vectorstore
+        retriever=store.as_retriever(search_type="similarity_score_threshold", search_kwargs={"score_threshold": .7, "k": 1}, metadata_field_info=metadata_field_info), # https://python.langchain.com/docs/modules/data_connection/retrievers/vectorstore
         chain_type_kwargs=chain_type_kwargs,
         return_source_documents=True,
         verbose=True
     )
-    answer = qa_chain({"question": question})
+
+    await qa_chain.acall(inputs={"question": query})
+
+# request input format
+class Query(BaseModel):
+    text: str
+
+async def create_gen(query: str, stream_it: AsyncCallbackHandler):
+    task = asyncio.create_task(run_call(query, stream_it))
+    async for token in stream_it.aiter():
+        yield token
+    await task
+
+@app.get("/chat")
+async def chat(
+    query: Query = Body(...),
+):
+    stream_it = AsyncCallbackHandler()
+    gen = create_gen(query.text, stream_it)
+    return StreamingResponse(gen, media_type="text/event-stream")
+
+@app.get("/health")
+async def health():
+    """Check the api is running"""
+    return {"status": "🤙"}
 
 
-    responses = answer
-
-    return responses['answer']
-
-
-
+##################################################################################### Videos
 # Funktion zum Streamen von Videodateien
 def video_streamer(video_path: str):
     with open(video_path, "rb") as video_file:
@@ -163,4 +175,12 @@ async def stream_video( lecture: str, video_name: str):
         video_streamer(video_path),
         media_type="video/mp4",
         headers={"Content-Disposition": f"attachment;filename={video_name}.mp4"},
+    )
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="localhost",
+        port=8000,
+        reload=True
     )
