fbf16a1c949cc5efae1de99a4b88bef1612dc9a3
Sven J <51181752+SvenJacobsUni@users.noreply.github.com>
2023-11-27 11:35:29 +0100

    Lecture Linker: Docker & Docker-Compose Setup
    - Setup everything with one command.
    - Important: All the video files are not in this repo!!
    
    current problem: video stream not working once the backend is hostet on the server (works fine locally...)

--- LectureLinker/app/client_angular/src/environments/environment.ts
+++ LectureLinker/app/client_angular/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-   server: 'http://localhost:8000',
-   wsserver: 'ws://localhost:8000'
-   //server: 'https://api.tutor-kai.de'
+   //server: 'http://localhost:8000',
+   //wsserver: 'ws://localhost:8000'
+      server: 'http://api-lecturelinker.bshefl0.bs.informatik.uni-siegen.de:3000',
+      wsserver: 'ws://api-lecturelinker.bshefl0.bs.informatik.uni-siegen.de:3000'
   };
