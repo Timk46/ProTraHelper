4b84ce869cd780d27ba6a03ff8da483d82f36212
timkr <tim.krenz@student.uni-siegen.de>
2024-03-07 12:48:43 +0100

    added single evaluation, push to pull TODO: implement evaluation and running collections from postman via nestjs service (instead of a seperate project like now)

--- server_nestjs/src/mcqcreation/mcqcreation.module.ts
+++ server_nestjs/src/mcqcreation/mcqcreation.module.ts
@@ -5,10 +5,12 @@ import { McqcreationController } from './mcqcreation.controller';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { JsonLoaderService } from './jsonloader.service';
 import { HttpModule } from '@nestjs/axios';
+import { EvaluationService } from './evaluation.service';
+import { EvaluationController } from './evaluation.controller';
 
 @Module({
   imports: [PrismaModule, HttpModule],
-  providers: [McqCreationService, JsonLoaderService],
-  controllers: [McqcreationController]
+  providers: [McqCreationService, JsonLoaderService, EvaluationService],
+  controllers: [McqcreationController, EvaluationController]
 })
 export class McqCreationModule {}
