1b349e569cbcf1cc5e9d77b519ed8d71708e560a
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2024-10-15 10:36:29 +0200

    fix(graph-evaluation): handle case for no solution submitted

--- server_nestjs/src/graph-solution-evaluation/graph-solution-evaluation.service.ts
+++ server_nestjs/src/graph-solution-evaluation/graph-solution-evaluation.service.ts
@@ -18,6 +18,10 @@ export class GraphSolutionEvaluationService {
 
     evaluateSolution(question: GraphQuestionDTO, studentSolution: GraphStructureDTO[]) {
 
+        if (!studentSolution || studentSolution.length === 0) {
+            return { feedback: 'Keine Lösung abgegeben.', receivedPoints: 0 };
+        }
+
         // Some questions types require evaluating a single-step solution, while others require a multi-step solution 
         // For single-step solutions, use the first step from the solution array `studentSolution[0]` `expectedSolution[0]`, 
         // and for multi-step solutions, use the entire array `studentSolution`, `expectedSolution`
