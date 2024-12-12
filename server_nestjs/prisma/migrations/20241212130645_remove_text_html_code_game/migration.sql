91f1adca9416af376be7edb37cada9da486ce2df
Felix Thalmann <felix.thalmann@online.de>
2024-12-12 19:03:05 +0100

    CodeGame: removed not needed textHTML table attribute at CodeGameQuestion table

---
+++ server_nestjs/prisma/migrations/20241212130645_remove_text_html_code_game/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `textHTML` on the `CodeGameQuestion` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "CodeGameQuestion" DROP COLUMN "textHTML";
