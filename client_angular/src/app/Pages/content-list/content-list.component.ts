cfc40e560b970523fcab2096690fad978ebbfc52
Jappaha <140712936+Jappaha@users.noreply.github.com>
2025-01-14 15:16:10 +0100

    content list component init
    new component to replace the content board component

--- client_angular/src/app/Pages/content-list/content-list.component.ts
+++ client_angular/src/app/Pages/content-list/content-list.component.ts
@@ -1,10 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ContentsForConceptDTO } from '@DTOs/index';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-content-list',
   templateUrl: './content-list.component.html',
-  styleUrls: ['./content-list.component.scss']
+  styleUrls: ['./content-list.component.scss'],
+  animations: [
+    trigger('contentExpand', [
+      state('collapsed', style({
+        height: '0',
+        opacity: 0,
+        overflow: 'hidden'
+      })),
+      state('expanded', style({
+        height: '*',
+        opacity: 1
+      })),
+      transition('collapsed <=> expanded', animate('200ms ease-out'))
+    ])
+  ]
 })
 export class ContentListComponent {
 
