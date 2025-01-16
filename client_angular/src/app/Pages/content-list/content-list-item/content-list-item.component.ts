fde87c9a66a84ddad570340d6e459c2017f05773
Jappaha <140712936+Jappaha@users.noreply.github.com>
2025-01-16 01:21:56 +0100

    content-list: elements and cards
    + added correct content enumerations
    + added task cards inside the content-list panels
    o stylized elements

--- client_angular/src/app/Pages/content-list/content-list-item/content-list-item.component.ts
+++ client_angular/src/app/Pages/content-list/content-list-item/content-list-item.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ContentElementDTO, contentElementType, questionType } from '@DTOs/index';
 
 @Component({
   selector: 'app-content-list-item',
@@ -6,5 +7,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-list-item.component.scss']
 })
 export class ContentListItemComponent {
+
+  @Input() contentElementData: ContentElementDTO = {
+    id : -1,
+    type: contentElementType.TEXT,
+    positionInSpecificContentView: -1,
+    question: {
+      id: -1,
+      type: 'task',
+      level: -1,
+      progress: -1,
+      name: '',
+      description: '',
+    }
+  };
+
+  constructor() {
+
+  }
+
+  /**
+   * Generates a more readable name for a given question type.
+   *
+   * @param type - The type of the question.
+   * @returns A string representing the more readable name of the question type.
+   */
+  getQuestionTypeReadable(type: string | undefined): string {
+    switch (type) {
+      case questionType.MULTIPLECHOICE:
+        return 'Multiple Choice';
+      case questionType.SINGLECHOICE:
+        return 'Single Choice';
+      case questionType.FREETEXT:
+        return 'Freitext';
+      case questionType.FILLIN:
+        return 'Lückentext';
+      case questionType.CODE:
+        return 'Programmieraufgabe';
+      case questionType.GRAPH:
+        return 'Graphaufgabe';
+      default:
+        return 'Aufgabe';
+    }
+  }
+
+  /**
+   * Returns the corresponding icon name for a given question type.
+   *
+   * @param type - The type of the question. It can be one of the following:
+   * @returns The name of the icon corresponding to the given question type.
+   */
+  getQuestionTypeIcon(type: string | undefined): string {
+    switch (type) {
+      case questionType.MULTIPLECHOICE:
+        return 'list';
+      case questionType.SINGLECHOICE:
+        return 'radio_button_checked';
+      case questionType.FREETEXT:
+        return 'text_fields';
+      case questionType.FILLIN:
+        return 'short_text';
+      case questionType.CODE:
+        return 'code';
+      case questionType.GRAPH:
+        return 'device_hub';
+      default:
+        return 'help';
+    }
+  }
 
 }
--- client_angular/src/app/Pages/content-list/content-list.component.ts
+++ client_angular/src/app/Pages/content-list/content-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ContentsForConceptDTO } from '@DTOs/index';
+import { ContentDTO, ContentElementDTO, contentElementType, ContentsForConceptDTO } from '@DTOs/index';
 import { animate, state, style, transition, trigger } from '@angular/animations';
+import { ContentElement } from '@DTOs/prisma.dto';
 
 @Component({
   selector: 'app-content-list',
@@ -29,9 +30,56 @@ export class ContentListComponent {
     requiredBy: [],
   };
 
+  expandedIndex = 0;
+  panelOpenState = false;
+
   // emitter for refreshing
   @Output() fetchContentsForConcept = new EventEmitter<void>();
 
+  constructor() { }
+
+  /**
+   * Generates an array with a specified number of elements.
+   *
+   * @param num - The number of elements in the array.
+   * @returns An array with the specified number of elements.
+   */
+  getLevels(num: number) {
+    return new Array(num);
+  }
+
+
+  /**
+   * Checks if the given content has at least one content element of the specified type.
+   *
+   * @param content - The content object to check.
+   * @param type - The type of content element to look for.
+   * @returns `true` if the content has at least one element of the specified type, otherwise `false`.
+   */
+  hasContentElementType(content: ContentDTO, type: string): boolean {
+    return content.contentElements.some((element) => element.type === type);
+  }
+
+  /**
+   * Filters and returns the content elements of type QUESTION from the given content.
+   *
+   * @param {ContentDTO} content - The content object containing content elements.
+   * @returns {ContentElement[]} An array of content elements that are of type QUESTION.
+   */
+  getQuestions(content: ContentDTO): ContentElementDTO[] {
+    return content.contentElements.filter((element) => element.type === contentElementType.QUESTION);
+  }
+
+  /**
+   * Retrieves the attachments from the given content.
+   * Filters the content elements to include only those of type PDF or VIDEO.
+   *
+   * @param content - The content object containing content elements.
+   * @returns An array of content elements that are either of type PDF or VIDEO.
+   */
+  getAttachments(content: ContentDTO): ContentElementDTO[] {
+    return content.contentElements.filter((element) => element.type === contentElementType.PDF || element.type === contentElementType.VIDEO);
+  }
 
 
 }
