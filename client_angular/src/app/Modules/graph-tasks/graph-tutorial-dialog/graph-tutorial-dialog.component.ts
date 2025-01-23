4fa46d5fe8f979dee0d9dad51cf6190d950e9337
AhmetTanerKahraman <kahramanahmettaner@gmail.com>
2025-01-23 13:59:11 +0100

    feat(graph-tasks): enhance GraphTutorialDialog with video selection and improved layout
    - Updated GraphTutorialDialog to support multiple tutorial videos with a streamlined video selection feature.
    - Improved the layout of the tutorial dialog for a better user experience.
    - Added dedicated tutorial videos for each graph task type, allowing users to access task-specific guidance.

--- client_angular/src/app/Modules/graph-tasks/graph-tutorial-dialog/graph-tutorial-dialog.component.ts
+++ client_angular/src/app/Modules/graph-tasks/graph-tutorial-dialog/graph-tutorial-dialog.component.ts
@@ -9,6 +9,32 @@ export class GraphTutorialDialogComponent implements OnDestroy {
 
   @ViewChild('videoPlayer') videoPlayer!: ElementRef;
 
+  videosFolder = '../../../assets//videos';
+  tutorials = [
+    {
+      title: 'Graphaufgaben',
+      videoUrl: `${this.videosFolder}/goals_graphaufgaben_tutorial.mp4`,
+    },
+    {
+      title: 'Dijkstra',
+      videoUrl: `${this.videosFolder}/goals_dijkstra_tutorial.mp4`,
+    },
+    {
+      title: 'Floyd',
+      videoUrl: `${this.videosFolder}/goals_floyd_tutorial.mp4`,
+    },
+    {
+      title: 'Transitive Hülle',
+      videoUrl: `${this.videosFolder}/goals_transitive_hülle_tutorial.mp4`,
+    },
+    {
+      title: 'Kruskal',
+      videoUrl: `${this.videosFolder}/goals_kruskal_tutorial.mp4`,
+    },
+  ]
+
+  currentTutorial = 0;
+
   // preventing Premature close conncetion error by closing the modal while video is loading
   ngOnDestroy() {
     this.stopAndResetVideo();
