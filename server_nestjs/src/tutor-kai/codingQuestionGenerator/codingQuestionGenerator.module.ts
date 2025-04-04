4b702e7b62d8d8db2e6ada10f60727764cb131cf
Sven J. <51181752+SvenJacobsUni@users.noreply.github.com>
2025-04-04 19:46:30 +0200

    remove old codingQuestionGeneratorCPP.service

--- server_nestjs/src/tutor-kai/codingQuestionGenerator/codingQuestionGenerator.module.ts
+++ server_nestjs/src/tutor-kai/codingQuestionGenerator/codingQuestionGenerator.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CodingQuestionGeneratorService } from './codingQuestionGeneratorPython.service';
-import { CodingQuestionGeneratorCppService } from './codingQuestionGeneratorCPP.service';
 import { CodingQuestionGeneratorController } from './codingQuestionGenerator.controller';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from '../../prisma/prisma.module';
 import { RunCodeModule } from '../run-code/run-code.module';
 @Module({
-  providers: [CodingQuestionGeneratorService,  CodingQuestionGeneratorCppService],
+  providers: [CodingQuestionGeneratorService],
   controllers: [CodingQuestionGeneratorController],
   imports: [RunCodeModule, PrismaModule, ConfigModule.forRoot()],
 })
