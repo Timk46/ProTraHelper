f807f036a29f38299da69b385587949ac1389197
Jappaha <140712936+Jappaha@users.noreply.github.com>
2025-06-15 19:38:06 +0200

    highlight navigator component
    + adding a third navigation component dummy called "highlight-navigation"

---
+++ client_angular/src/app/Pages/highlight-navigator/highlight-navigator.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HighlightNavigatorComponent } from './highlight-navigator.component';
+
+describe('HighlightNavigatorComponent', () => {
+  let component: HighlightNavigatorComponent;
+  let fixture: ComponentFixture<HighlightNavigatorComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HighlightNavigatorComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HighlightNavigatorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client_angular/src/app/Pages/highlight-navigator/highlight-navigator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-highlight-navigator',
+  standalone: true,
+  imports: [],
+  templateUrl: './highlight-navigator.component.html',
+  styleUrl: './highlight-navigator.component.scss'
+})
+export class HighlightNavigatorComponent {
+
+}
